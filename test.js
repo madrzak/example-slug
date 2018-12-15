8f86e3cc734407e350a9d39b873934ac82972f81
Lukasz Madrzak <lukasz.madrzak@over-c.com>
2018-12-15 15:02:49 +0000

    init

---
+++ test.js
@@ -0,0 +1,18 @@
+window.$ = window.jQuery = require('jquery');
+
+
+var slugify = require('slugify');
+
+
+// window.speakingurl = require('speakingurl');
+// window.slugify = require('jquery-slugify');
+
+
+// window.$ = window.jQuery = require('speakingurl');
+// window.$ = window.jQuery = require('jquery-slugify');
+
+// var _ = require('speakingurl');
+// var _ = require('jquery-slugify');
+//
+// require('speakingurl');
+// require('jquery-slugify');
